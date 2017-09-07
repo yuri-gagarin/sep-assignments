2a987de28d32ffc6ee8fe9bd44ce4c781c7c4223
Yuriy Ivanov <ymivanov@icloud.com>
2017-09-07 15:29:48 -0400

    Stack and Queue complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -3,16 +3,32 @@ class MyQueue
   attr_accessor :tail
 
   def initialize
+
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
+
   end
 
   def enqueue(element)
+    @queue.push(element)
+    @head = @queue[0]
+    @tail = @queue.last
+
   end
 
   def dequeue
+
+    dequeued_el = @queue.shift
+    @head = @queue[0]
+    @tail = @queue.last
+    return dequeued_el
+
   end
 
   def empty?
+
+    @queue.empty? ? true : false
+    
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -2,16 +2,48 @@ class MyStack
   attr_accessor :top
 
   def initialize
+
     @stack = Array.new
     self.top = nil
+
   end
 
   def push(item)
+
+    @stack.push(item)
+    self.top = item
+
+  end
+
+  def display_stack
+
+    puts @stack
+
   end
 
   def pop
+
+    top_item = self.top
+    @stack.delete(top)
+    self.top = @stack.last
+    return top_item
+
   end
 
   def empty?
+
+    @stack.empty? ? true : false
+
   end
+
 end
+
+stack1 = MyStack.new()
+stack1.push(2)
+stack1.push(4)
+stack1.push(10)
+stack1.push(-5)
+
+puts stack1.empty?
+stack1.pop
+stack1.display_stack
