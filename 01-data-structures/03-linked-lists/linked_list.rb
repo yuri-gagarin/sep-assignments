e73ad8516ba950b8bb0e03e931c8af5c58cbc52b
Yuriy Ivanov <ymivanov@icloud.com>
2017-09-22 08:12:21 -0400

    complete linked list

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -6,25 +6,101 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+
+    current = @head
+    if (@head ==nil) && (@tail == nil)
+      @head = node 
+      @tail = node 
+    else
+      while current.next != nil
+        current = current.next
+      end
+      current.next = node 
+      @tail = node
+      @tail.next = nil
+    end
+    
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+
+    current = @head
+    #if no tail do nothing
+    if @tail == nil
+      return nil
+    elsif @head == @tail 
+      @head = nil
+      @tail = nil
+    #else go down the list and remove the tail
+    else
+      while (current.next != nil) && (current.next != @tail)
+        current = current.next
+      end
+      @tail = current
+      @tail.next = nil
+    end
+
   end
 
   # This method prints out a representation of the list.
   def print
+    #print the head
+    current = @head
+    puts current.data
+    #go down the list and print every object
+    while current.next != nil
+      current = current.next 
+      puts current.data
+    end
+
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+
+    current = @head
+    if @tail == nil
+      return nil
+      #list is empty / do nothing
+    elsif @head == node 
+      @head = @head.next
+      #if tail is the node remove the tail
+    elsif @tail == node
+      remove_tail
+    else
+      while (current.next != nil) && (current.next != node)
+        current = current.next
+      end
+      current.next = current.next.next
+    end
+
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+
+    if (@head == nil) && (@tail == nil)
+      @head = node
+      @head.next = nil
+      @tail = node
+      @tail.next = nil
+    else
+      temp = @head
+      @head = node
+      @head.next = temp
+    end
+
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+
+    if (@head == nil) || (@head.next == nil) || (@head == @tail)
+      @head = nil
+    else
+      @head = @head.next
+    end
   end
+
 end
