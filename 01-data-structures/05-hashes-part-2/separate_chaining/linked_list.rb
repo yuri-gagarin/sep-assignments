010578a0f3ba685cc265450805b0b69da3b58954
Yuriy Ivanov <ymivanov@icloud.com>
2017-10-26 14:18:48 -0400

    linked list for separate chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,71 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    current = @head
+    if @head == nil
+      @head = node
+      @tail = node
+    else
+      node.prev = @tail
+      @tail.next = node
+      @tail = @tail.next
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if @tail == @head
+      @head = nil
+      @tail = nil
+    else
+      @tail = @tail.prev
+      @tail.next = nil
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    current = @head
+    until current == nil do
+      puts current.data
+      node = node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      @head = node.next
+      @head.prev = nil
+    elsif @tail == node
+      @tail = node.prev
+      @tail.next = nil
+    else
+      node.prev.next = node.next
+      node.next.prev = node.prev
+      node = nil
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    if @head == nil
+      @head = node
+    else
+      node.next = @head
+      @head = node
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    if @head.next == nil
+      @head = nil
+      @tail = nil
+    else
+      @head = @head.next
+      @head.prev = nil
+    end
   end
+  
 end
